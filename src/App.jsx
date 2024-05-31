758c99e1696a4666ca4a625a9ad42a81118d5c63
Daniela Nina <danielaninacastillo@gmail.com>
2024-05-31 12:50:51 -0400

    InicioC

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import MensajeA from './componente/MensajeA'
+import Timer from './componentes/Timer'
+import Encabezado from './componentes/Encabezado'
+import Button from './componentes/Button'
+function App() {
+
+  return (
+    <>
+      {/* <MensajeA/> */}
+      {/* <Encabezado/> */}
+      {/* <Timer/>  */}
+      <Button/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componente/MensajeA.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import "./MensajeA.css"
+function MensajeA (){
+    return (
+        
+        <div className="ContenedorP">
+            <div className="Contenedor">
+                <img src="./assets/mensaje.png" alt="" />
+                <div className="contenedorLetra">
+                    <h1>CENTAMAX FIX</h1>
+                    <p>Cada gota de sudor,cada repetición y cada minuto en el gimnasio 
+                        es una inversión en tu salud y bienestar. 
+                        ¡Tú puedes!</p>
+                </div>
+            </div>
+        
+        </div>
+    )
+}
+export default MensajeA;
