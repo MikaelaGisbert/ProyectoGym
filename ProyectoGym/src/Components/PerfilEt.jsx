d39a8321ea45cdf578ee8a1e6055e0b7df292c1b
adalidwa <adalidjesusxd@gmail.com>
2024-06-01 21:37:51 -0400

    Historia 18: aplicado atimic a los componetes

--- ProyectoGym/src/Components/PerfilEt.jsx
+++ ProyectoGym/src/Components/PerfilEt.jsx
@@ -1,36 +1,15 @@
 import React from 'react'
-import Img from "../assets/FotoEt1.png"
+import PerfilProfecionalEntrenador from "./PerfilProfecionalEntrenador.jsx";
+import NameFotoEntrenador from './NameFotoEntrenador.jsx';
+import ContactosEntrenador from './ContactosEntrenador.jsx';
 import './PerfilEt.css'
 
 const PerfilE = () => {
   return (
     <div className='Perfil'>
-        <div className='FotoNameIng'>
-            <div className='Name'>
-                <h1>MARIO HUGO HERRERA QUISPE</h1>
-            </div>
-            <div className='ImagenName'>
-                <img src={Img} alt="" />
-            </div>
-            <hr />
-        </div>
-        <div className='DatosPerf'>
-            <h3>PERFIL PROFECIONAL</h3>
-            <p>Personal trainer con grado en Ciencias de la actividad fisica y del 
-                deporte y experiencia acreditable de trabajo en centros de fitness
-                impartiendo diversas especialidades tanto grupales como individuales 
-                solidos conocimientos de fisioterapia y rehabilitacion de lesiones
-            </p>
-
-            <hr />
-        </div>
-        <div className='Contactos'>
-            <h3>Contacto</h3>
-            <p>
-                el:68148899
-                maherrersua@gmail.co
-            </p>
-        </div>
+        <PerfilProfecionalEntrenador/>
+        <NameFotoEntrenador/>
+        <ContactosEntrenador/>
     </div>
   )
 }
